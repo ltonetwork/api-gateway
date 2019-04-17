aee6c498722e3c3287f24743b86ece46e5d854d9
sven4ask <sven@legalthings.io>
2019-04-17 13:36:53 +0200

    Allow cors calls

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { LoggerService } from './logger/logger.service';
 import { ConfigService } from './config/config.service';
+import cors from 'cors';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -9,6 +11,9 @@ async function bootstrap() {
   });
   const logger = await app.get<LoggerService>(LoggerService);
   const config = await app.get<ConfigService>(ConfigService);
+
+  app.use(cors());
+  app.use(helmet());
 
   await app.init();
   await app.listen(config.getPort());
